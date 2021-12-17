0c133256af84415155b377a9786ed17733c82738
Ali <contact@alibellamine.me>
2021-12-17 09:40:12 +0100

    Local change

--- test.py
+++
@@ -1,9 +0,0 @@
-from deepChess import stockFish
-
-opponent = stockFish.stockFish_connector("../stockfish_14.1_linux_x64_avx2/stockfish_14.1_linux_x64_avx2")
-
-print(
-    opponent.get_board()
-)
-
-
