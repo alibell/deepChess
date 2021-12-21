6fe4b217c7bf08ab1a52e429051dfc79c05a4dc1
Ali <contact@alibellamine.me>
2021-12-21 11:23:32 +0100

    train nn script

--- trainNN.py
+++ trainNN.py
@@ -13,6 +13,7 @@ import os
 import torch
 import glob
 import pickle
+import time
 
 # +
 #
@@ -24,10 +25,11 @@ import pickle
 data_folder = "/home/ali/deepChess/games"
 model_folder = "/home/ali/deepChess/models"
 log_folder = "/home/ali/deepChess/logs"
-lr = 0.01
-device = "cuda:0"
+lr = 0.01 # Learning rate of the training
+device = "cuda:0" # Device in which the model is loaded
 timer = 60*10 # Timer in seconds before attempting again to train the network
 max_epoch = 100 # Number of time it can has seen the same data
+max_model = 100 # Maximum number of model record to keep
 
 # +
 #
@@ -48,7 +50,7 @@ model.optimizer.param_groups[0]['lr'] = lr
 games = glob.glob(data_folder+"/**/*.pickle", recursive=True)
 
 # Getting train history
-train_history_path = data_folder+"/history.pickle"
+train_history_path = data_folder+"/history"
 
 if os.path.exists(train_history_path):
     history = pickle.load(open(train_history_path, "rb"))
@@ -59,37 +61,49 @@ else:
 # For each, we load the data and train the NN from it
 # -
 
-for game in games:
-    if game not in history.keys():
-        history[game] = 0
+while True:
+    for game in games:
+        if game not in history.keys():
+            history[game] = 0
+
+        if history[game] < max_epoch:
+            print(f"Pass n°{history[game]+1} on {game}")
+
+            # Loading data
+            game_data = pickle.load(open(game, "br"))
+            X = tuple(torch.tensor(x,dtype = torch.float32, device = device) for x in game_data[1])
+            moves = [torch.tensor(
+                    playChess._localToNNMove(None, [x[0]], game_data[0], 0)[1], dtype = torch.float32, device = device).flatten()
+                         for x in game_data[2]]
+            rewards = [torch.tensor(x[1], dtype = torch.float32, device = device) for x in game_data[2]]
+
+            X = (
+                torch.stack([X[0] for x in range(len(moves))]),
+                torch.stack([X[1] for x in range(len(moves))])
+            )
+            y = (torch.stack(rewards), torch.stack(moves))
+
+            # Fitting the model
+            model.fit(X, y)
+
+            # Saving the fit in history
+            history[game] += 1
+
+            # Saving the new model
+            next_model_path = "/".join([
+                model_folder,
+                str(int(".".join(model_path.split("/")[-1].split(".")[0:-1]))+1)
+            ])+".pt"
+            model_path = next_model_path
+            model.save(model_path)
+
+            # Saving the history
+            with open(train_history_path, "wb") as file:
+                pickle.dump(history, file)
     
-    if history[game] <= max_epoch:
-        print(f"Pass n°{history[game]} on {game}")
-        
-        # Loading data
-        game_data = pickle.load(open(game, "br"))
+    # Only keeping the last models
+    last_models = get_lastest_model(model_folder, max_model)
+    for model_to_delete in [x for x in glob.glob(model_folder+"/*.pt") if x not in last_models]:
+        os.remove(model_to_delete)
 
-        X = tuple(torch.tensor(x,dtype = torch.float32, device = device) for x in game_data[1])
-        moves = [torch.tensor(
-                playChess._localToNNMove(None, [x[0]], game_data[0], 0)[1], dtype = torch.float32, device = device).flatten()
-                     for x in game_data[2]]
-        rewards = [torch.tensor(x[1], dtype = torch.float32, device = device) for x in game_data[2]]
-
-        X = (
-            torch.stack([X[0] for x in range(len(moves))]),
-            torch.stack([X[1] for x in range(len(moves))])
-        )
-        y = (torch.stack(rewards), torch.stack(moves))
-
-        # Fitting the model
-        model.fit(X, y)
-
-        # Saving the fit in history
-        history[game] += 1
-        
-        # Saving the new model
-        next_model_path = "/".join([
-            model_folder,
-            str(int(".".join(model_path.split("/")[-1].split(".")[0:-1]))+1)
-        ])+".pt"
-        torch.save(next_model_path)
+    time.sleep(timer)
